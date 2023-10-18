eed31bc5beb8038632ad5479915ae1e30fbb24b4
PedroM42 <78854984+PedroM42@users.noreply.github.com>
2023-10-18 17:50:25 +0200

    Add files via upload

---
+++ cviko4/Core/Inc/adc.h
@@ -0,0 +1,13 @@
+/*
+ * adc.h
+ *
+ *  Created on: Oct 18, 2023
+ *      Author: xmedek08
+ */
+
+#ifndef INC_ADC_H_
+#define INC_ADC_H_
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc);
+
+#endif /* INC_ADC_H_ */
---
+++ cviko4/Core/Src/adc.c
@@ -0,0 +1,16 @@
+/*
+ * adc.c
+ *
+ *  Created on: Oct 18, 2023
+ *      Author: xmedek08
+ */
+
+#ifndef SRC_ADC_C_
+#define SRC_ADC_C_
+
+#include "main.h"
+#include "adc.h"
+
+
+
+#endif /* SRC_ADC_C_ */
